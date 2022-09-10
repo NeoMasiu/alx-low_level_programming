ee7a1f75fb0d11f529d1d2c700c1ae06538f2334
NeoMasiu <neo.masiu@bothouniversity.com>
2022-09-10 08:14:14 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,12 @@
 /**
   * main - Prints all single digit numbers of base 10 starting from 0.
   *
-  * eturn: Always 0.
+  * return: Always 0.
   */
 
 int main(void)
 {
-int num ;
+int num;
 
 for (num = 0; num < 10; num++)
 printf("%d", num);
