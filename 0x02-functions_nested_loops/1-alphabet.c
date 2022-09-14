d90034733e146168a2b4b556401f4cf7c85aa817
NeoMasiu <neo.masiu@bothouniversity.com>
2022-09-13 17:43:20 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+/*
+*Task 1
+* By Masiu
+*/
+
+#include "main.h"
+#include "_putchar.h"
+/**
+* print_alphabet - prints the english alphabet from a-z.
+*
+* Return: Nothing.
+*/
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
