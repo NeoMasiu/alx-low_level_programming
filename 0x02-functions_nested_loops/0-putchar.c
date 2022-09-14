5720fe967b92de9aa9f339dda54fb51935a21082
NeoMasiu <neo.masiu@bothouniversity.com>
2022-09-14 02:20:03 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,10 @@
-/*
-*
-*  Task 0
-*  By Masiu
-*/
-#include <main.h>
-/**                                                                                                      *main - prints the string "_putchar" from a character array.
-*
-* Return: 0 on success.
-*/
+#include "holberton.h"
 
+/**
+* main - prints the string "_putchar" from a character array.
+*
+*  Return: 0 on success.
+*/
 int main(void)
 {
 char c[] = "_putchar";
